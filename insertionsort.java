36e8d3220fb4a247e91586f271c7a9cf9365171f
sinchanagowri <139755634+sinchanagowri@users.noreply.github.com>
2023-08-10 22:16:12 +0530

    Update insertionsort.java

--- insertionsort.java
+++ insertionsort.java
@@ -1,7 +1,6 @@
 import java.util.Scanner;
 
 public class insertionsort {
-	// Java program for implementation of Insertion Sort
 	
 		void sort(int arr[])
 		{
@@ -10,10 +9,7 @@ public class insertionsort {
 				int key = arr[i];
 				int j = i - 1;
 
-				/* Move elements of arr[0..i-1], that are
-				greater than key, to one position ahead
-				of their current position */
-				while (j >= 0 && arr[j] > key) {
+				        while (j >= 0 && arr[j] > key) {
 					arr[j + 1] = arr[j];
 					j = j - 1;
 				}
@@ -21,8 +17,7 @@ public class insertionsort {
 			}
 		}
 
-		/* A utility function to print array of size n*/
-		static void printArray(int arr[])
+		 static void printArray(int arr[])
 		{
 			int n = arr.length;
 			for (int i = 0; i < n; ++i)
@@ -31,7 +26,6 @@ public class insertionsort {
 			System.out.println();
 		}
 
-		// Driver method
 		public static void main(String args[])
 		{
 			
